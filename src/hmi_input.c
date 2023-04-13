d859f8c4f43a3d06faa32145a32b3912f128bfd4
Wu Jinkang <wujinkang2000@gmail.com>
2023-04-13 18:56:06 +0200

    Add work in progress

--- src/hmi_input.c
+++ src/hmi_input.c
@@ -1,3 +1,52 @@
-int main () {
-    return 1;
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+
+int getCWD();
+
+int main (void) 
+{
+    getCWD();
+    int fd, messageLen, i, pid;
+    char message [100];
+
+    pid = getpid();
+
+    sprintf(message,"Hello from PID %d", pid); /* Prepare message */
+
+    messageLen = strlen(message) + 1;
+    int c = 0;
+    do 
+    { /* Keep trying to open the file until successful */
+        fd = open("aPipe", O_WRONLY); /* Open named pipe for writing */
+        printf("Attempt to open aPipe %d. (%d)\n", fd, c++);
+        if (fd == -1)
+            sleep(1); /* Try again in 1 second */ 
+    } while (fd == -1);
+
+    for (i = 1; i <= 10; i++) 
+    {   /* Send three messages */
+        /* Write message down pipe */
+        int res = write(fd, message, messageLen);
+        printf("Attempt to write on aPipe %d. counter(%d) status(%d)\n", fd, i, res);
+        sleep(3); /* Pause a while */
+    }
+    close(fd); /* Close pipe descriptor */
+    return 0; 
+}
+
+int getCWD() {
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) 
+    {
+        printf("Current working dir: %s\n", cwd);
+    } 
+    else 
+    {
+        perror("getcwd() error");
+        return 1;
+    }
+    return 0;
 }
--- src/hmi_output.c
+++ src/hmi_output.c
@@ -1,3 +1,82 @@
-int main () {
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *getCWD();
+int readLine(int fd, char *str);
+
+int main (void) 
+{
+    int fd;
+    char str[100];
+    char *cwd = getCWD();
+    char *filename = "/hmi_output";
+    size_t len = strlen(cwd) + strlen(filename) + 1;
+    char *fullpath = malloc(len);
+    if (fullpath == NULL) {
+        free(cwd);
+        perror("malloc() error");
+        return -1;
+    }
+    strcpy(fullpath, cwd);
+    free(cwd);
+    strcat(fullpath, filename);
+    printf("Full path: %s\n", fullpath);
+    free(fullpath);
+
+    /* Remove named pipe if it already exists */
+    unlink(fullpath);
+    /* Create named pipe */
+    mknod("aPipe", S_IFIFO, 0);
+    /* Change its permissions */
+    chmod("aPipe", 0660);
+
+    fd = open("aPipe", O_RDONLY); /* Open it for reading */ 
+    printf("fd status %d\n", fd);
+
+    /* Display received messages */
+    while(readLine (fd, str)) 
+    {
+        printf ("%s\n", str);
+    }
+
+    close(fd); /* Close pipe */ 
+    unlink("aPipe"); /* Remove used pipe */
+
     return 1;
+}
+
+char *getCWD() {
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        size_t len = strlen(cwd);
+        char *result = malloc(len + 1);
+        if (result == NULL) {
+            perror("malloc() error");
+            exit(-1);
+        }
+        strcpy(result, cwd);
+        return result;
+    } else {
+        perror("getcwd() error");
+        exit(-1);
+    }
+}
+
+int readLine(int fd, char *str) 
+{
+    /* Read a single ’\0’-terminated line into str from fd */
+    /* Return 0 when the end-of-input is reached and 1 otherwise */
+    int n;
+    do 
+    {   /* Read characters until ’\0’ or end-of-input */
+        n = read(fd, str, 1); /* Read one character */ 
+    } while (n > 0 && *str++ != '\0');
+
+    return (n > 0); /* Return false if end-of-input */
 }
