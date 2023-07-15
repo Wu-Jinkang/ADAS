9a4ae1ca984f57a1e4847c30060ab0fb57ffd4d8
Wu Jinkang <wujinkang2000@gmail.com>
2023-07-15 17:11:51 +0200

    Add inc

--- inc/util.h
+++ inc/util.h
@@ -1,7 +1,7 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-int readLineFromIndex(int fd, char *str, int *index);
+void readLineFromIndex(int fd, char *str, int *index);
 int writeln(int fd, char *str);
 int readLine(int fd, char *str);
 char* getDataSrcUrandom(char *mode);
